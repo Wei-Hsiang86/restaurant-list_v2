202b760463d17ee5f4174e4e8e3b983384d7d874
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-01-01 17:04:09 +0800

    setting router, install & init sequelize

--- app.js
+++ app.js
@@ -12,8 +12,34 @@ app.get("/", (req, res) => {
 
 app.get("/restaurants", (req, res) => {
   return Restaurant.findAll()
-    .then((rss) => res.send({ rss }))
+    .then((rest) => res.send({ rest }))
     .catch((err) => res.status(422).json(err));
 });
 
-app.listen
+app.get("/restaurants/new", (req, res) => {
+  res.send("create rest");
+});
+
+app.post("/restaurants", (req, res) => {
+  res.send("add new rest");
+});
+
+app.get("/restaurants/:id", (req, res) => {
+  res.send(`get restaurant: ${req.params.id}`);
+});
+
+app.get("/restaurants/:id/edit", (req, res) => {
+  res.send(`get restaurant edit: ${req.params.id}`);
+});
+
+app.put("/restaurants/:id", (req, res) => {
+  res.send("modify restaurant");
+});
+
+app.delete("/restaurants/:id", (req, res) => {
+  res.send("delete restaurant");
+});
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`);
+})
